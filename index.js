d8692c28cf0dc133fd35d4ac4b572e4653d771d4
jibon-roy <mailme.jibon@gmail.com>
2023-12-22 13:55:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express();
+const port = process.env.PORT | 5000
+
+
+app.use(
+    cors({
+        origin: ['http://localhost:5173', 'http://localhost:5174',],
+    }),
+)
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://taskerbeta:FuGTXEjkalqDoUjI@cluster0.ziw2dg7.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const TaskerDB = client.db('TaskerBeta')
+        const usersCollection = TaskerDB.collection('users')
+
+        app.post('/createUser', (req, res) => {
+            console.log(req.body)
+        })
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running.')
+})
+
+app.listen(port, () => {
+    console.log(`Server running port: ${port}`)
+})
